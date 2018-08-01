0bf1307950689b066840a2a33e5fad634170e835
Arnim Bleier <arnim.bleier@gmail.com>
2018-08-01 16:45:08 +0200

    new dependencies

--- binder/install.R
+++ binder/install.R
@@ -1,1 +1,4 @@
 install.packages("stringr", repos = "https://cloud.r-project.org/", dependencies=TRUE)
+install.packages("data.table", repos = "https://cloud.r-project.org/", dependencies=TRUE)
+install.packages("psych", repos = "https://cloud.r-project.org/", dependencies=TRUE)
+install.packages("tidyverse", repos = "https://cloud.r-project.org/", dependencies=TRUE)
